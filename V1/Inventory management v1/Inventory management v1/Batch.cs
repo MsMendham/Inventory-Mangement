eada749d3a56351b9c80e983240895b5f2c8c4aa
MsMendham <bmwilliamson4@gmail.com>
2022-12-02 16:20:02 +0000

    created invent menu cclasses

---
+++ V1/Inventory management v1/Inventory management v1/Batch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_management_v1
+{
+    public class Batch // creates class
+    {
+        private int BatchNo;
+        private object Item;
+        private int expiryDate;
+        private int Quantity;
+        public Batch(object item, int Number, int Date, int Quant) // constructor method
+        {
+            BatchNo = Number;
+            Item = item;
+            expiryDate = Date;
+            Quantity = Quant;
+        }
+        
+        public int getNumber() // getter and setter methods
+        {
+            return BatchNo;
+        }
+        public object getItem()
+        {
+            return Item;
+        }
+        public int getDate()
+        {
+            return expiryDate;
+
+        }
+        public int getQuantity()
+        {
+            return Quantity;
+        }
+
+        public void setItem(object item)
+        {
+            this.Item = item;
+        }
+        public void setDate(int date)
+        {
+            this.expiryDate= date;
+        }
+        public void setQuantity(int quant)
+        {
+            this.Quantity = quant;
+        }
+
+    }
+}
---
+++ V1/Inventory management v1/Inventory management v1/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_management_v1 
+{
+    public class Item // creates the item class
+    {
+        private int itemID; // defines the attributes
+        private string itemName;
+        private string itemType;
+        public Item(int ID, string Name, string Type) // constructor method for object creation
+        {
+            itemID = ID;
+            itemName = Name;
+            itemType = Type;
+        }
+
+        public int getID() // get and setter methods for attribute access and changing 
+        {
+            return itemID;
+        }
+        public string getName()
+        {
+            return itemName;
+        }
+        public string getType()
+        {
+            return itemType;
+        }
+        public void setName(string Name)
+        {
+            itemName = Name;
+        }
+        public void setType(string Type)
+        {
+            itemType = Type;
+        }
+    }
+}
